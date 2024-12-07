ce52ff7355a494ab7a05fe7a2cb6089c8cdc26ba
Archit <archit9spicin@gmail.com>
2024-12-07 15:07:27 +0530

    changes

--- src/components/Dropdown/index.jsx
+++ src/components/Dropdown/index.jsx
@@ -84,7 +84,7 @@ const Dropdown = ({
                 <span> {dropDownName}</span>
               )}
             </div>
-            <div>{""}</div>
+            <div>{" "}</div>
           </>
         )}
       </div>
